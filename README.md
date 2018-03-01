c53d9b4d2515f1518815188d936238c09ea46b3f
Alexander Dietmüller <alexander@dietmueller.de>
2018-03-01 22:48:57 +0100

    Initial files from template.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# amiv_bouncer
+# AMIV Bouncer
+
 Update AMIV API memberships with the member list provided by VSETH.
+
+```
+npm install
+
+# dev server
+npm start
+
+# lint
+npm run lint
+
+# build
+npm run build
+```
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Go for it! :)
+
+const title = document.createElement('h1');
+title.innerHTML = 'Hello World.';
+
+const main = document.createElement('p');
+main.innerHTML = 'Bounce bounce.';
+
+document.body.appendChild(title);
+document.body.appendChild(main);
